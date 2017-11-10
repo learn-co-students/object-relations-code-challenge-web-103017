531a01e2dae94cc8453a04147e4c4140e99ec83e
Silvia Chung <silviachung@Silvias-MacBook.local>
2017-11-10 12:06:26 -0500

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,29 @@
 class Restaurant
-  attr_accessor :name
+
+  attr_accessor :name, :reviews
+
+  @@all = [  ]
 
   def initialize(name)
     @name = name
+    @@all << self
+    @reviews = []
   end
 
+  def self.find_by_name(string_name) #given a string of restaurant name, returns the first restaurant that matches
+    Restaurant.all.find { |restaurant| restaurant.name == string_name}
+  end
+
+
+  def customers #should return all of the customers who have written reviews of that restaurant.
+
+  end
+
+  def self.all #returns an array of all restaurants
+    @@all
+  end
+  
+  # def reviews #returns an array of all reviews for that restaurant
+  #
+  # end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,17 @@
 require_relative '../config/environment.rb'
-
+require 'pry'
 def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+
+jack = Customer.new('Jack', 'Luuu')
+sally = Customer.new('Sally', 'Woo')
+chipotle = Restaurant.new('chiptole')
+mcdonald = Restaurant.new('mcdonald')
+review1 = Review.new("yummmm")
+review1 = Review.new("delishhh")
+
+binding.pry
+
+review1
