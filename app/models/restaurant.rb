36afbdf5fa430120a32a7b00838d8e3aec419484
github username <github email address>
2017-11-10 12:04:53 -0500

    Finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
   attr_accessor :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+
+    @@all
+
+  end
+
+  def self.find_by_name(name)
+    @@all.select{|restaurant| restaurant.name == name}
+
+  end
+
+
+  def reviews
+
+    Review.all.select{|review| review.resturant == self}
+
+  end
+
+
+  def customers
+    reviews = Review.all.collect{|review| review}
+    revs_for_rest = reviews.select{|rev| rev.resturant == self }
+    revs_for_rest.collect{|item| item.customer}
+
   end
 
 end
+
+
+# + Restaurant.all
+#   + returns an array of all restaurants
+# + Restaurant.find_by_name(name)
+#   + given a string of restaurant name, returns the first restaurant that matches
+# + Restaurant#reviews
+#   + returns an array of all reviews for that restaurant
+# + Restaurant#customers
+#   + should return all of the customers who have written reviews of that restaurant.
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
-  
+  @@all = []
+  attr_accessor :customer, :resturant, :content
+
+  def initialize(customer, resturant, content)
+    @customer = customer
+    @resturant = resturant
+    @content = content
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
 
+# + Review.all
+#   + returns all of the reviews
+# + Review#customer
+#   + returns the customer for that given review
+# + Review#restaurant
+#   + returns the restaurant for that given review
