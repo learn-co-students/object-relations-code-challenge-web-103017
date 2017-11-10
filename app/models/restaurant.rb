32b86e2a8be5a4caaad4250aef36015312e72149
c0nniewang <connie.wang303@gmail.com>
2017-11-10 12:00:36 -0500

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
+  @@all = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    Review.all.map do |review|
+      if review.restaurant == self
+        review.customer
+      end
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,23 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+
+connie = Customer.new("connie", "wang")
+andy = Customer.new("andy", "tien")
+jenny = Customer.new("jenny", "chen")
+andy2 = Customer.new("andy", "smith")
+connie2 = Customer.new("connie", "wang")
+
+
+kenka = Restaurant.new("kenka")
+ngam = Restaurant.new("ngam")
+
+review = connie.add_review(kenka, "blahblah")
+review2 = jenny.add_review(ngam, "moreblah")
+review3 = andy.add_review(kenka, "blahblahblah")
+
+
+
+
+binding.pry
+true
