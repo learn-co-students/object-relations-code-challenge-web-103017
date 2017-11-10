aa8adb4861710f949e02ca9d25e9ab6a9af3e66a
github username <github email address>
2017-11-10 12:00:42 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
-end
+
+  # Return all of the customer instances
+
+  def self.all
+    # if the only customers we care about are those who have written a review
+    # Review.all.collect{|ri| ri.customer}.uniq
+    @@all
+  end
+
+  # Given a string of a full name , return the first customer whose full name matches
+  def self.find_by_name(name)
+    self.all.find{|instance| instance.full_name == name}
+  end
+
+  #given a string of a first name,
+  # Return an array containing all customers with that first name
+  def self.find_all_by_first_name(name)
+    self.all.select{|instance| instance.first_name == name}
+  end
+
+  # return an array of all full names
+  def self.all_names
+    self.all.collect{|instance| instance.full_name}
+  end
+
+
+  def add_review(restaurant, content)
+    Review.new(self, restaurant, content)
+  end
+
+  def reviews
+    Review.all.select{|instance| instance.customer == self}
+  end
+end # => End class
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
 
+  @@all = []
+  attr_accessor :customer, :restaurant, :content
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
+
+end
