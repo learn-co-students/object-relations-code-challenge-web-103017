381f03a9ac6946541454b3e120a2fef97e6361eb
github username <github email address>
2017-11-10 12:02:51 -0500

    finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
-  
-end
 
+  attr_accessor :customer, :restaurant, :customer_instance, :restaraunt_instance, :review
+  @@all = []
+
+  def initialize(customer_instance, restaraunt_instance, review)
+    @customer_instance = customer_instance
+    @restaraunt_instance = restaraunt_instance
+    @review = review
+    @@all << self
+  end
+
+  def customer
+    self.customer_instance
+  end
+
+  def restaurant
+    self.restaraunt_instance
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,29 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+
+
+brendan = Customer.new('Brendan', 'McIlhenny')
+carol = Customer.new('Carol', 'McIlhenny')
+brian = Customer.new('Brian', 'McIlhenny')
+
+
+sal_n_joes = Restaurant.new('Sal N Joes')
+han = Restaurant.new('Han Dynasty')
+panera = Restaurant.new('Panera')
+
+brendan_sal_n_joes_review = Review.new(brendan, sal_n_joes, 'It was good')
+carol_panera_review = Review.new(carol, panera, 'It was ok')
+brian_panera_review = Review.new(brian, panera, 'It was meh')
+brendan_han_review = Review.new(brendan, han, 'It was spicy')
+
+
+brendan.add_review(panera, 'love the chicken soup')
+brendan.full_name
+
+
+Customer.find_by_name('Brendan McIlhenny')
+Customer.all_names
+binding.pry
+
+Restaurant.find_by_name('Han Dynasty')
