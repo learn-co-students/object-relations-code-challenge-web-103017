b0e1df571a203e1f67db7266357ecb10ec2f656f
dansbands <dansbands@hotmail.com>
2017-11-10 12:04:14 -0500

    not done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
+
+  @@all = []
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    # X returns array of all restaurants
+    @@all
+  end
+
+  def self.find_by_name(name)
+    # X given a string of restaurant name, returns the first restaurant that matches
+    self.all.find{ |restaurant| restaurant.name == name }
+  end
+
+  def reviews
+    # returns an array of all reviews for that restaurant
+
+    
+  end
+
+  def customers
+    # should return all of the customers who have written reviews of that restaurant.
+
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review
-  
-end
 
+    @@all = []
+
+  attr_accessor :customer, :restaurant, :content
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+  end
+
+  def self.all
+    @@all
+  end
+end
