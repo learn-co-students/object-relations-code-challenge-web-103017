703a106304f3b46f5e73a46f32a9843ba8dc4766
lierica <ericaaa.li@gmail.com>
2017-11-10 12:01:29 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
-  attr_accessor :first_name, :last_name
+
+@@all_customers = []
+
+  attr_accessor :first_name, :last_name, :full_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all_customers << self
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{@first_name} #{@last_name}"
   end
+
+  def self.all
+    @@all_customers
+  end
+
+  def self.all_names
+    @@all_customers.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all_customers.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def self.find_by_name(name)
+    @@all_customers.select do |customer|
+      @full_name = customer.full_name
+      @full_name == name
+    end
+  end
+
+  def add_review(restaurant, content)
+    Review.new(restaurant, content, self)
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
-  attr_accessor :name
+
+@@all_restaurants = []
+
+  attr_accessor :name, :customer
 
   def initialize(name)
     @name = name
+    @@all_restaurants << self
+  end
+
+  def self.all
+    @@all_restaurants
+  end
+
+  def self.find_by_name(name)
+    @@all_restaurants.select do |restaurant|
+      restaurant.name == name
+    end.first
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    review_customers = Review.all.select do |review|
+      review.restaurant == self
+    end
+    review_customers.map do |review|
+      review.customer
+    end
   end
 
 end
