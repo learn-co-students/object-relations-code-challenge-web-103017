c1b91847667b26cf96a9f968a7dae5d45e6b2570
github username <github email address>
2017-11-10 12:01:37 -0500

    Seth Completed the Code challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,39 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+    @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    first_name = name.split(" ")[0]
+    last_name = name.split(" ")[1]
+    self.all.find {|customer| customer.first_name == first_name && customer.last_name == last_name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select {|customer| customer if customer.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map {|customer| "#{customer.first_name} #{customer.last_name}"}
+  end
+
+  def add_review(restaurant, content)
+    Review.new(restaurant, self, content)
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,22 @@ def reload
   load 'config/environment.rb'
 end
 
+
+mud = Restaurant.new("Mud")
+veselka = Restaurant.new("Veselka")
+mud2 = Restaurant.new("Mud")
+mermaid = Restaurant.new("Mermaid Inn")
+
+seth = Customer.new("Seth", "Barden")
+tim = Customer.new("Tim", "Meadows")
+seth2 = Customer.new("Seth", "Smith")
+bob = Customer.new("Bob", "Sagat")
+
+seth.add_review(mud, "Sucks")
+seth.add_review(veselka, "GREAT!")
+tim.add_review(mermaid, "so-so")
+tim.add_review(mud, "terrific")
+bob.add_review(mud2, "Better than regular mud")
+
+
 Pry.start
